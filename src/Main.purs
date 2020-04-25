b04d366b215349a7ae7c9954d7628f87afdb24c8
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-04-26 01:29:00 +0200

    Add a few more simple functions to test things

--- src/Main.purs
+++ src/Main.purs
@@ -1,19 +1,59 @@
 module Main where
 
 import Prelude
-import Data.String (toLower, joinWith, split, trim)
+import Data.Maybe (Maybe(..))
+import Data.String (joinWith, split, stripPrefix, stripSuffix, toLower, trim)
 import Data.String.Pattern (Pattern(..))
 import Data.String.Regex (replace)
 import Data.String.Regex.Flags (global, ignoreCase)
 import Data.String.Regex.Unsafe (unsafeRegex)
 
--- import Effect (Effect)
--- import Effect.Console (log)
+-- | Strip any characters that are not ascii lowercase letters or numbers.
 strip :: String -> String
 strip = replace (unsafeRegex "[^a-z0-9+]+" (global <> ignoreCase)) ""
 
+-- | Strip any repeating dashes from a string.
 stripDash :: String -> String
 stripDash = replace (unsafeRegex "--+" global) "-"
 
+-- | Creates a slug for some string.
+-- |
+-- | ```purescript
+-- | >>> slugify "foo bar baz"
+-- | "foo-bar-baz"
+-- | >>> slugify "fo''o b%$!ar baz,  " 
+-- | "foo-bar-baz"
+-- | ```
 slugify :: String -> String
 slugify str = stripDash <<< joinWith "-" <<< map strip <<< split (Pattern " ") <<< toLower $ trim str
+
+-- | Strips `public/` from a string.
+-- |
+-- | ```purescript
+-- | >>> stripPublic "public/hello/world/"
+-- | "hello/world/"
+-- | >>> stripPublic "/hello/world/"
+-- | "/hello/world/"
+-- | ```
+stripPublic :: String -> String
+stripPublic str = case stripPrefix (Pattern "public/") str of
+  Just out -> out
+  Nothing -> str
+
+-- | Fixes the slug of some string by removing the leading `/` of it and 
+-- | appending a `/` if it is missing.
+-- |
+-- | ```purescript
+-- | >>> fixSlug "/about" 
+-- | "about/"
+-- | >>> fixSlug "about" 
+-- | "about/"
+-- | >>> fixSlug "/about/" 
+-- | "about/"
+-- | ```
+fixSlug :: String -> String
+fixSlug str = case stripPrefix (Pattern "/") str, stripSuffix (Pattern "/") str of
+  Just out, Just _ -> out
+  Just out, Nothing -> out <> "/"
+  Nothing, Just _ -> str
+  _, _ -> str <> "/"
--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,7 @@ module Test.Main where
 import Prelude
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Main (slugify)
+import Main (slugify, stripPublic, fixSlug)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
@@ -15,9 +15,32 @@ main =
     $ runSpec [ consoleReporter ] do
         describe "slugify" do
           it "replaces simple strings" do
-            (slugify "foo bar baz") `shouldEqual` "foo-bar-baz"
+            slugify "foo bar baz" `shouldEqual` "foo-bar-baz"
           it "removes trailing whitespace" do
-            (slugify "   foo bar   baz  ") `shouldEqual` "foo-bar-baz"
+            slugify "   foo bar   baz  " `shouldEqual` "foo-bar-baz"
           it "removes illegal characters" do
-            (slugify "foo, bar. baz!") `shouldEqual` "foo-bar-baz"
-            (slugify "fo''o b%$!ar baz,  ") `shouldEqual` "foo-bar-baz"
+            slugify "foo, bar. baz!" `shouldEqual` "foo-bar-baz"
+            slugify "fo''o b%$!ar baz,  " `shouldEqual` "foo-bar-baz"
+          it "strips dashes correctly" do
+            slugify "-foo-- bar, baz" `shouldEqual` "foo-bar-baz"
+          it "strips" do
+            slugify "Hello, world! It's a glorious" `shouldEqual` "hello-world-its-a-glorious"
+            slugify "this_ IS a % of $dollars" `shouldEqual` "this-is-a-of-dollars"
+            slugify "1 is equal == to ---3" `shouldEqual` "1-is-equal-to-3"
+        describe "strip public" do
+          it "strips `public/`" do
+            stripPublic "public/assets/index.be69cc27.css" `shouldEqual` "assets/index.be69cc27.css"
+            stripPublic "public/about/index.html" `shouldEqual` "about/index.html"
+            stripPublic "public/index.html" `shouldEqual` "index.html"
+            stripPublic "public/" `shouldEqual` ""
+          it "does not strip `/public/`" do
+            stripPublic "/public/" `shouldEqual` "/public/"
+        describe "fix slug" do
+          it "strip beginning `/`" do
+            fixSlug "/about" `shouldEqual` "about/"
+            fixSlug "/" `shouldEqual` ""
+          it "appends ending `/`" do
+            fixSlug "about" `shouldEqual` "about/"
+          it "strips beginning `/` and appends `/`" do
+            fixSlug "/about/" `shouldEqual` "about/"
+            fixSlug "about/" `shouldEqual` "about/"
